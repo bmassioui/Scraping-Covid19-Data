a1da3f0e47d4d0c77949fb1e7a68fcc191a76777
bouchaib-massioui <massioui.bouchaib@uit.ac.ma>
2021-02-22 13:03:40 -0800

    search is working with columns index instead of

--- backend/repository.py
+++ backend/repository.py
@@ -10,13 +10,12 @@ def get():
     df = pd.read_csv(datasource_path)
     return df.head()
 
-# get record for world
+# get record for ""world
 def get_4_world():
     print('start')
     df = get()
     print(df)
-    df.loc[df["Country,Other"] == "World"]
+    # df.loc[df[""Country,Other"] == "World"]
+    print(df[df["7"] == "ActiveCases"])
     print('end')
     # return df.head()
-
-
