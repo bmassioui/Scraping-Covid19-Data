407353cad00c632c5431de6e80599aaf719246a8
bouchaib-massioui <massioui.bouchaib@uit.ac.ma>
2021-02-24 11:18:34 -0800

    get al method api

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,6 @@
 # expose scraped data for covid19
 from flask import Flask
+from flask import request
 import scraper
 import helpers
 import constants
@@ -8,50 +9,55 @@ import repository
 app = Flask(__name__)
 
 # returns all scraped data as JSON result
+
+
 @app.route('/get', methods=["GET"])
 def get():
     try:
         if(not helpers.is_datasource_exists()):
-            return {'status code' : 412, 'Message' : 'Datasource unavailable, please refresh datasource!'}
+            return {'status code': 412, 'Message': 'Datasource unavailable, please refresh datasource!'}
 
         return repository.getall()
     except:
-        return {'status code' : 500, 'Message' : 'Internal Server Error, please contact administrator.'}
+        return {'status code': 500, 'Message': 'Internal Server Error, please contact administrator.'}
 
 # returns single record of WORLD's
+
+
 @app.route('/get_4_world', methods=["GET"])
 def get_4_world():
     try:
         if(not helpers.is_datasource_exists()):
-            return {'status code' : 412, 'Message' : 'Datasource unavailable, please refresh datasource!'}
+            return {'status code': 412, 'Message': 'Datasource unavailable, please refresh datasource!'}
 
         return repository.get_4_world()
     except:
-        return {'status code' : 500, 'Message' : 'Internal Server Error, please contact administrator.'}
+        return {'status code': 500, 'Message': 'Internal Server Error, please contact administrator.'}
 
 # returns country's records
+
+
 @app.route('/get_4_country', methods=["GET"])
-def get_4_country(country):
-   try:
+def get_4_country():
+    country = request.args.get('country')
+    try:
+        if(not country):
+            return {'status code': 404, 'Message': 'No data has been found!'}
+
         if(not helpers.is_datasource_exists()):
-            return {'status code' : 412, 'Message' : 'Datasource unavailable, please refresh datasource!'}
+            return {'status code': 412, 'Message': 'Datasource unavailable, please refresh datasource!'}
 
-        if()
-            return {'status code' : 404, 'Message' : 'No data has been found!'}
-
-
-        return repository.get_4_world()
+        return repository.get_4_country(country)
     except:
-        return {'status code' : 500, 'Message' : 'Internal Server Error, please contact administrator.'}
+        return {'status code': 500, 'Message': 'Internal Server Error, please contact administrator.'}
 
 # refresh DATASOURCE(excel file .csv)
+
+
 @app.route('/refresh', methods=["GET"])
 def refresh():
     try:
         scraper.process()
-        return {'status code' : 200, 'Message' : 'Datasource has been refreshed successfully!'}
+        return {'status code': 200, 'Message': 'Datasource has been refreshed successfully!'}
     except:
-        return {'status code' : 500, 'Message' : 'Unable to scrape covid19 data.'}
-
-
-    
+        return {'status code': 500, 'Message': 'Unable to scrape covid19 data.'}
